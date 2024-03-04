e7f19c4afce8c6d3867d81d4b520b0e027ee3a19
madhushani-29 <madhushani2904@gmail.com>
2024-03-04 15:16:18 +0530

    fitrebase

--- lib/core/errors/server_failure.dart
+++
@@ -1,7 +0,0 @@
-abstract class Failure{}
-
-class ServerFailure implements Failure{
-  final String message;
-
-  ServerFailure(this.message);
-}
--- lib/src/task/data/data_sources/todo_remote_data_source.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:core';
-import 'package:todo/src/task/data/models/todo-model.dart';
-
-abstract class TodoRemoteDataSource {
-  Future<List<TodoModel>> getAllTodo();
-  Future<void> createTodo(TodoModel todo);
-  Future<void> updateTodo(TodoModel todo);
-  Future<bool> deleteTodo(int id);
-}
