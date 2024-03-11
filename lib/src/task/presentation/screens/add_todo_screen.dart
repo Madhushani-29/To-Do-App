3b6b1ffceccef050cb93e55183804ea4215a19de
madhushani-29 <madhushani2904@gmail.com>
2024-03-11 16:32:20 +0530

    add todo refactor

--- lib/src/task/presentation/screens/add_todo_screen.dart
+++ lib/src/task/presentation/screens/add_todo_screen.dart
@@ -24,7 +24,7 @@ class _TodoCreateScreenState extends State<TodoCreateScreen> {
     final createTodoBloc = BlocProvider.of<TodoBloc>(context);
     return Scaffold(
         appBar: AppBar(),
-        body: BlocConsumer<TodoBloc, TodoState>(
+        body: BlocListener<TodoBloc, TodoState>(
           listener: (context, state) {
             if (state is TodoSuccessful) {
               ScaffoldMessenger.of(context).showSnackBar(SnackBar(
@@ -41,153 +41,145 @@ class _TodoCreateScreenState extends State<TodoCreateScreen> {
               ));
             }
           },
-          builder: (context, state) {
-            if (state is TodoLoading) {
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            }
-
-            return ListView(
-              padding: const EdgeInsets.symmetric(horizontal: 16),
-              children: [
-                Form(
-                  key: _formKey,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const Text(
-                        AppStrings.titleLabel,
-                        style: TextStyle(
-                            color: AppColors.tertiaryTextColor,
-                            fontSize: 14,
-                            fontWeight: FontWeight.w500),
-                      ),
-                      TextFormField(
-                        autovalidateMode: AutovalidateMode.always,
-                        maxLength: 20,
-                        decoration: const InputDecoration(hintText: "title"),
-                        onChanged: (text) {
-                          setState(() {
-                            _title = text;
-                          });
-                        },
-                      ),
-                      const Text(
-                        AppStrings.dateLabel,
-                        style: TextStyle(
-                            color: AppColors.tertiaryTextColor,
-                            fontSize: 14,
-                            fontWeight: FontWeight.w500),
-                      ),
-                      TextFormField(
-                        autovalidateMode: AutovalidateMode.always,
-                        maxLength: 20,
-                        decoration: const InputDecoration(hintText: "date"),
-                        onChanged: (text) {
-                          setState(() {
-                            _date = text;
-                          });
-                        },
-                      ),
-                      const Text(
-                        AppStrings.priorityLabel,
-                        style: TextStyle(
-                            color: AppColors.tertiaryTextColor,
-                            fontSize: 14,
-                            fontWeight: FontWeight.w500),
-                      ),
-                      DropdownButtonHideUnderline(
-                        child: DropdownButton2<String>(
-                          isExpanded: true,
-                          hint: const Text(
-                            AppStrings.dropDownButtonText,
-                            style: TextStyle(
-                              fontSize: 12,
-                              fontWeight: FontWeight.w400,
-                              color: AppColors.buttonTextColor,
-                            ),
-                            overflow: TextOverflow.ellipsis,
+          child: ListView(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            children: [
+              Form(
+                key: _formKey,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      AppStrings.titleLabel,
+                      style: TextStyle(
+                          color: AppColors.tertiaryTextColor,
+                          fontSize: 14,
+                          fontWeight: FontWeight.w500),
+                    ),
+                    TextFormField(
+                      autovalidateMode: AutovalidateMode.always,
+                      maxLength: 20,
+                      decoration: const InputDecoration(hintText: "title"),
+                      onChanged: (text) {
+                        setState(() {
+                          _title = text;
+                        });
+                      },
+                    ),
+                    const Text(
+                      AppStrings.dateLabel,
+                      style: TextStyle(
+                          color: AppColors.tertiaryTextColor,
+                          fontSize: 14,
+                          fontWeight: FontWeight.w500),
+                    ),
+                    TextFormField(
+                      autovalidateMode: AutovalidateMode.always,
+                      maxLength: 20,
+                      decoration: const InputDecoration(hintText: "date"),
+                      onChanged: (text) {
+                        setState(() {
+                          _date = text;
+                        });
+                      },
+                    ),
+                    const Text(
+                      AppStrings.priorityLabel,
+                      style: TextStyle(
+                          color: AppColors.tertiaryTextColor,
+                          fontSize: 14,
+                          fontWeight: FontWeight.w500),
+                    ),
+                    DropdownButtonHideUnderline(
+                      child: DropdownButton2<String>(
+                        isExpanded: true,
+                        hint: const Text(
+                          AppStrings.dropDownButtonText,
+                          style: TextStyle(
+                            fontSize: 12,
+                            fontWeight: FontWeight.w400,
+                            color: AppColors.buttonTextColor,
                           ),
-                          items: AppStrings.priorityListItems
-                              .map((String item) => DropdownMenuItem<String>(
-                                    value: item,
-                                    child: Text(
-                                      item,
-                                      style: const TextStyle(
-                                        fontSize: 12,
-                                        fontWeight: FontWeight.w400,
-                                        color: AppColors.buttonTextColor,
-                                      ),
-                                      overflow: TextOverflow.ellipsis,
-                                    ),
-                                  ))
-                              .toList(),
-                          value: _priority,
-                          onChanged: (value) {
-                            setState(() {
-                              _priority = value;
-                            });
-                          },
-                          buttonStyleData: ButtonStyleData(
-                            height: 45,
-                            width: double.infinity,
-                            padding: const EdgeInsets.only(left: 30, right: 20),
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(5),
-                              border: Border.all(
-                                color: AppColors.dropdownBorderColor,
-                              ),
-                              color: AppColors.primaryButtonBackgroundColor,
-                            ),
-                            elevation: 2,
-                          ),
-                          iconStyleData: const IconStyleData(
-                            icon: Icon(
-                              Icons.arrow_drop_down_sharp,
-                            ),
-                            iconSize: 12,
-                            iconEnabledColor: AppColors.primaryButtonIconColor,
-                            iconDisabledColor: AppColors.primaryButtonIconColor,
-                          ),
-                          dropdownStyleData: DropdownStyleData(
-                            width: MediaQuery.of(context).size.width - 32,
-                            maxHeight: 140,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(5),
-                              color: AppColors.primaryButtonBackgroundColor,
-                            ),
-                            offset: const Offset(0, 0),
-                            scrollbarTheme: ScrollbarThemeData(
-                              radius: const Radius.circular(0),
-                              thickness: MaterialStateProperty.all(5),
-                              thumbVisibility: MaterialStateProperty.all(true),
-                            ),
-                          ),
-                          menuItemStyleData: const MenuItemStyleData(
-                              padding: EdgeInsets.only(left: 30, right: 20),
-                              height: 30),
+                          overflow: TextOverflow.ellipsis,
                         ),
-                      ),
-                      ElevatedButton(
-                        onPressed: () {
-                          TodoData todo = TodoData(
-                            id: '',
-                            title: _title ?? '',
-                            date: _date ?? '',
-                            priority: _priority ?? '',
-                            status: 'pending',
-                          );
-                          createTodoBloc.add(CreateTodos(todo: todo));
+                        items: AppStrings.priorityListItems
+                            .map((String item) => DropdownMenuItem<String>(
+                                  value: item,
+                                  child: Text(
+                                    item,
+                                    style: const TextStyle(
+                                      fontSize: 12,
+                                      fontWeight: FontWeight.w400,
+                                      color: AppColors.buttonTextColor,
+                                    ),
+                                    overflow: TextOverflow.ellipsis,
+                                  ),
+                                ))
+                            .toList(),
+                        value: _priority,
+                        onChanged: (value) {
+                          setState(() {
+                            _priority = value;
+                          });
                         },
-                        child: const Text(AppStrings.createButtonHint),
+                        buttonStyleData: ButtonStyleData(
+                          height: 45,
+                          width: double.infinity,
+                          padding: const EdgeInsets.only(left: 30, right: 20),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(5),
+                            border: Border.all(
+                              color: AppColors.dropdownBorderColor,
+                            ),
+                            color: AppColors.primaryButtonBackgroundColor,
+                          ),
+                          elevation: 2,
+                        ),
+                        iconStyleData: const IconStyleData(
+                          icon: Icon(
+                            Icons.arrow_drop_down_sharp,
+                          ),
+                          iconSize: 12,
+                          iconEnabledColor: AppColors.primaryButtonIconColor,
+                          iconDisabledColor: AppColors.primaryButtonIconColor,
+                        ),
+                        dropdownStyleData: DropdownStyleData(
+                          width: MediaQuery.of(context).size.width - 32,
+                          maxHeight: 140,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(5),
+                            color: AppColors.primaryButtonBackgroundColor,
+                          ),
+                          offset: const Offset(0, 0),
+                          scrollbarTheme: ScrollbarThemeData(
+                            radius: const Radius.circular(0),
+                            thickness: MaterialStateProperty.all(5),
+                            thumbVisibility: MaterialStateProperty.all(true),
+                          ),
+                        ),
+                        menuItemStyleData: const MenuItemStyleData(
+                            padding: EdgeInsets.only(left: 30, right: 20),
+                            height: 30),
                       ),
-                    ],
-                  ),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        TodoData todo = TodoData(
+                          id: '',
+                          title: _title ?? '',
+                          date: _date ?? '',
+                          priority: _priority ?? '',
+                          status: 'pending',
+                        );
+                        createTodoBloc.add(CreateTodos(todo: todo));
+                      },
+                      child: const Text(AppStrings.createButtonHint),
+                    ),
+                  ],
                 ),
-              ],
-            );
-          },
+              ),
+            ],
+          ),
         ));
   }
 }
