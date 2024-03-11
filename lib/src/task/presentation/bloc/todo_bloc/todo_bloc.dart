42c76b1a48d1653ec1af79b567f6b5c1392f997f
madhushani-29 <madhushani2904@gmail.com>
2024-03-11 16:35:09 +0530

    add todo refactor

--- lib/src/task/presentation/bloc/todo_bloc/todo_bloc.dart
+++ lib/src/task/presentation/bloc/todo_bloc/todo_bloc.dart
@@ -5,7 +5,7 @@ import 'package:todo/src/task/presentation/Models/todo.dart';
 part 'todo_event.dart';
 part 'todo_state.dart';
 
-class TodoBloc extends Bloc<TodoEvent, TodoState> {
+class TodoBloc extends Bloc<TodoCreateEvent, TodoState> {
   final CreateTodo createTodo;
   TodoBloc({required this.createTodo})
       : super(TodoInitial()) {
--- lib/src/task/presentation/bloc/todo_bloc/todo_event.dart
+++ lib/src/task/presentation/bloc/todo_bloc/todo_event.dart
@@ -1,19 +1,11 @@
 part of 'todo_bloc.dart';
 
-sealed class TodoEvent {}
+sealed class TodoCreateEvent {}
 
-class CreateTodos extends TodoEvent {
+class CreateTodos extends TodoCreateEvent {
   final TodoData todo;
 
   CreateTodos({required this.todo});
-  @override
-  List<Object?> get props => [todo];
-}
-
-class UpdateTodos extends TodoEvent {
-  final TodoData todo;
-
-  UpdateTodos({required this.todo});
   @override
   List<Object?> get props => [todo];
 }
